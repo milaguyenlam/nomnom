7551bb232653f2359b1418a4bbfd65d435b2aafd
vutienvu <vutienvu@seznam.cz>
2023-07-15 17:51:42 +0200

    [mobile-app] feat: create distance text component

---
+++ mobile-client/components/DistanceText.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function DistanceText() {
+    return (
+        <View style={styles.distanceTextWrapper}>
+            <Text style={styles.distanceText}>400 m</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    distanceTextWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        marginTop: 20,
+        marginBottom: 20,
+    },
+    distanceText: {
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        paddingTop: 7,
+        paddingRight: 14,
+        paddingLeft: 14,
+        paddingBottom: 7,
+        borderRadius: 8,
+        color: '#fff',
+    },
+});
