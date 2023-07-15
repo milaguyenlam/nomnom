cdd602c9163c4da27fcdba341cd5c18ece9e8161
vutienvu <vutienvu@seznam.cz>
2023-07-15 20:58:20 +0200

    [mobile-app] feat: create action buttons component

---
+++ mobile-client/components/ActionButtons.tsx
@@ -0,0 +1,59 @@
+import { StyleSheet, View } from 'react-native'
+import React from 'react'
+
+import { Entypo } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+
+export default function ActionButtons() {
+  return (
+    <View style={styles.actionButtons}>
+        <View style={styles.skipButton}>
+        <Entypo name="cross" size={50} color="white" />
+        </View>
+        <View style={styles.sendButton}>
+        <Feather name="send" size={30} color="black" />
+        </View>
+        <View style={styles.favoriteButton}>
+        <AntDesign name="hearto" size={24} color="white" />
+        </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    actionButtons: {
+        flexDirection: 'row',
+        marginVertical: 25,
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+    },
+    skipButton: {
+        width: 55,
+        height: 55,
+        borderRadius: 1000,
+        backgroundColor: '#000',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    sendButton: {
+        width: 55,
+        height: 55,
+        borderRadius: 1000,
+        justifyContent: 'center',
+        alignItems: 'center',
+        elevation: 5,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 4,
+    },
+    favoriteButton: {
+        width: 55,
+        height: 55,
+        borderRadius: 1000,
+        backgroundColor: 'orange',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
