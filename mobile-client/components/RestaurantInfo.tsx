f7702ecc91a6ea80b050d9cd7f785e9633668bde
vutienvu <vutienvu@seznam.cz>
2023-07-15 20:31:38 +0200

    [mobile-app] feat: create res. info component

--- mobile-client/components/RestaurantInfo.tsx
+++ mobile-client/components/RestaurantInfo.tsx
@@ -2,14 +2,40 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import OpenStatus from './OpenStatus'
 import RestaurantName from './RestaurantName'
+import InformationIcon from './InformationIcon'
+import PriceLevel from './PriceLevel'
+import RatingText from './RatingText'
+import { Octicons } from '@expo/vector-icons';
 
 export default function RestaurantInfo() {
   return (
-    <View style={{backgroundColor: 'red'}}>
-      <OpenStatus />
-      <RestaurantName />
+    <View style={{ backgroundColor: 'rgba(0,0,0,0.2)' }}>
+        <OpenStatus />
+
+        <View style={styles.restaurantSection}>
+            <RestaurantName />
+            <InformationIcon />
+        </View>
+
+        <View style={styles.reviewsSection}>
+            <PriceLevel />
+            <Octicons name="dot-fill" size={8} color="white" style={styles.dot}/>
+            <RatingText />
+        </View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    restaurantSection: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    reviewsSection: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    dot: {
+        marginHorizontal: 12.5,
+    },
+});
