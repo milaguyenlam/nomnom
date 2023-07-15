54d8dab35afd249cf9f43d52e363c5e7070d20b0
vutienvu <vutienvu@seznam.cz>
2023-07-15 17:43:11 +0200

    [mobile-app] feat: create picture indicator

---
+++ mobile-client/components/PictureIndicator.tsx
@@ -0,0 +1,41 @@
+import { StyleSheet, View, Dimensions } from 'react-native';
+
+const numberOfLines = 4;
+
+export default function PictureIndicator() {
+    return (
+        <View style={styles.pictureIndicator}>
+            <View style={[styles.line, styles.furthestLeft, styles.currentLine]}></View>
+            <View style={[styles.line]}></View>
+            <View style={[styles.line]}></View>
+            <View style={[styles.line, styles.furthestRight]}></View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    pictureIndicator: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    line: {
+        flex: 1/numberOfLines,
+        marginLeft: 5,
+        marginRight: 5,
+        height: 5,
+        borderRadius: 1000,
+        backgroundColor: 'rgba(0,0,0,0.30)',
+    },
+    furthestLeft: {
+        marginLeft: 0,
+        marginRight: 5,
+    },
+    furthestRight: {
+        marginLeft: 5,
+        marginRight: 0,
+    },
+    currentLine: {
+        backgroundColor: '#fff',
+    },
+});
