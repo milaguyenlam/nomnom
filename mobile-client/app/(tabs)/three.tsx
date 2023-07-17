c51e4b078e870885f45f02863ef09134168cc29b
vutienvu <vutienvu@seznam.cz>
2023-07-17 16:02:18 +0200

    [mobile-app] test: create testing tab file

---
+++ mobile-client/app/(tabs)/three.tsx
@@ -0,0 +1,27 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function TabThreeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Tab three</Text>
+      <View style={styles.separator} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
