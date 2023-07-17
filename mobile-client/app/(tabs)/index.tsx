6db792719ff11a4370af06b558122c42a48d7b39
vutienvu <vutienvu@seznam.cz>
2023-07-17 15:22:37 +0200

    [mobile-app] docs: update router files

--- mobile-client/app/(tabs)/index.tsx
+++ mobile-client/app/(tabs)/index.tsx
@@ -1,31 +1,29 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View, SafeAreaView } from 'react-native';
+import ActionButtons from '../../components/ActionButtons';
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import Header from '../../components/Header';
+import PictureCard from '../../components/PictureCard';
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.homePage}>
+        <Header />
+        <PictureCard />
+        <ActionButtons />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  homePage: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingLeft: 15,
+    paddingRight: 15,
   },
 });
