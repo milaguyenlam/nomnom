b5954cf46664b4fa034286710f3ec96dfc19bb4a
vutienvu <vutienvu@seznam.cz>
2023-07-22 19:38:33 +0200

    [mobile-app] feat: add react query provider

--- mobile-client/app/_layout.tsx
+++ mobile-client/app/_layout.tsx
@@ -1,14 +1,19 @@
 import { Stack } from 'expo-router';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export default function RootLayoutNav() {
   return (
     // <ThemeProvider value={DarkTheme}>
+    <QueryClientProvider client={queryClient}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ 
           headerShown: false,
         }} />
       </Stack>
+    </QueryClientProvider>
     // </ThemeProvider>
   );
 }
