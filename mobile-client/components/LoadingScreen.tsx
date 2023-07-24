37924643f8bb7cb1c2f299f4b304883dd8144ff4
vutienvu <vutienvu@seznam.cz>
2023-07-24 17:24:06 +0200

    [mobile-app] feat: create loading screen

---
+++ mobile-client/components/LoadingScreen.tsx
@@ -0,0 +1,17 @@
+import { Dimensions, StyleSheet, Text, View,ActivityIndicator } from 'react-native'
+import React from 'react'
+
+export default function LoadingScreen() {
+  return (
+    <View style={styles.loadingScreenWrapper}>
+      <ActivityIndicator size={80} color='f95a00' />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    loadingScreenWrapper: {
+        height: Dimensions.get('window').height * 0.6,
+        justifyContent: 'center',
+    },
+});
