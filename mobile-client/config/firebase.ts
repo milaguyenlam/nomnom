243e49bfc9fc4b717d80177217fb4e19f5f93592
Nguyen Thanh Lamgit commit -m Initial <milaguyenlam@gmail.com>
2023-08-13 14:04:51 +0200

    fix: remove FIREBASE_DATABASE_URL variable

--- mobile-client/config/firebase.ts
+++ mobile-client/config/firebase.ts
@@ -5,7 +5,6 @@ import { getStorage } from "@firebase/storage";
 import {
   FIREBASE_API_KEY,
   FIREBASE_AUTH_DOMAIN,
-  FIREBASE_DATABASE_URL,
   FIREBASE_PROJECT_ID,
   FIREBASE_STORAGE_BUCKET,
   FIREBASE_MESSAGING_SENDER_ID,
@@ -15,7 +14,6 @@ import {
 const firebaseConfig = {
   apiKey: FIREBASE_API_KEY,
   authDomain: FIREBASE_AUTH_DOMAIN,
-  databaseURL: FIREBASE_DATABASE_URL,
   projectId: FIREBASE_PROJECT_ID,
   storageBucket: FIREBASE_STORAGE_BUCKET,
   messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
